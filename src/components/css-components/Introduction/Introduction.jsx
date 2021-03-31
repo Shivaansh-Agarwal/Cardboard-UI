929d3e8c052d6f8ef2ed73a4b31442be8d195af6
Shivaansh Agarwal <shivanshagrawal1997@gmail.com>
2021-03-31 12:24:04 +0530

    fix: Changing Text on Introduction Page

--- src/components/css-components/Introduction/Introduction.jsx
+++ src/components/css-components/Introduction/Introduction.jsx
@@ -4,6 +4,16 @@ const Introduction = () => {
   return (
     <div className="introduction">
       <h1>Welcome to Cardboard UI</h1>
+      <div>
+        A ReactJS based Frontend component library for building fast & latest
+        web-interfaces
+      </div>
+      <br />
+      <br />
+      <br />
+      <div>
+        <strong>By - Shivaansh Agarwal</strong>
+      </div>
     </div>
   );
 };
